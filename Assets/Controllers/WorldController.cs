259edb6cea9c7ad6d86e37b099c50a63fdc17f4f
mistelucat <monsieur.miaou@gmail.com>
2016-06-04 16:05:05 +0200

    Premier Commit tuto
    plante pas !

---
+++ Assets/Controllers/WorldController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldController : MonoBehaviour {
+	World world;
+
+	// Use this for initialization
+	void Start () {
+		world = new World ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Model/World.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class World {
+
+	Tile[,] tiles;
+	int width;
+	int height;
+
+	public World(int width = 100, int height = 100){
+		this.width = width;
+		this.height = height;
+
+		tiles = new Tile[width, height];
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				tiles[x, y] = new Tile (this, x, y);	
+			}
+		}
+		Debug.Log ("World cree avec " + (width * height) + " tiles.");
+	}
+
+	public Tile GetTileAt(int x, int y)  {
+		if( x > width || x < 0 || y > height || y < 0 ) {
+			Debug.LogError("tile ("+x+" , "+y+") is out of raaange.");
+				return null;
+				}
+		return tiles[x, y];
+	}
+
+}
